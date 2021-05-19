34865276b3468f27b8fe608cb6c7e93d9009a922
PUtkala <51992192+PUtkala@users.noreply.github.com>
2021-05-19 22:31:00 +0200

    Zrobione przejścia menu i opcje

---
+++ MiedzJamVertex/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    int sceneIndex = 1;
+    public void PlayGame(int SetIndex)
+    {    
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+
+}
