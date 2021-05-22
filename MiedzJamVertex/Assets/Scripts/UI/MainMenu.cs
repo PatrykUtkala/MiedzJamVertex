3cdd6a5a6d520a94cee16eecbd18cc3e615f3c1b
PUtkala <51992192+PUtkala@users.noreply.github.com>
2021-05-22 21:16:46 +0200

    ustawienia builda

--- MiedzJamVertex/Assets/Scripts/UI/MainMenu.cs
+++ MiedzJamVertex/Assets/Scripts/UI/MainMenu.cs
@@ -6,7 +6,8 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame(int SetIndex)
-    {    
+    {
+        Time.timeScale = 1;
         SceneManager.LoadScene(SetIndex);
     }
 
