e0c240582631f5a01619e45b5e0b1684df9af462
PUtkala <51992192+PUtkala@users.noreply.github.com>
2021-05-23 23:23:35 +0200

    Ostatnie poprawki, potrzebne jeszcze creditsy

--- MiedzJamVertex/Assets/Scripts/UI/MainMenu.cs
+++ MiedzJamVertex/Assets/Scripts/UI/MainMenu.cs
@@ -8,6 +8,7 @@ public class MainMenu : MonoBehaviour
     public void PlayGame(int SetIndex)
     {    
         SceneManager.LoadScene(SetIndex);
+        Time.timeScale = 1;
     }
 
     public void QuitGame()
